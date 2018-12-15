f8b20e640d160911616ee2df6eb11eaa2d828dee
ramrakeshasn <ramrakeshasn@gmail.com>
2018-12-15 18:14:49 -0500

    Added last name greeting support

--- src/com/ram/gitpractise/common/CommonUtil.java
+++ src/com/ram/gitpractise/common/CommonUtil.java
@@ -2,7 +2,8 @@ package com.ram.gitpractise.common;
 
 public class CommonUtil {
 
-	public static  final String name="Ram Rakesh"; 
+	public static  final String firstname="Ram Rakesh";
+	public static  final String lastname="ASN";
 	
 
 }
